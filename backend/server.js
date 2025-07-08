e34e6b1ac2adc1fd1a02c9dc4ff77d7e7fb926a4
Gabriel Alvise <schenorberger123@gmail.com>
2025-07-08 00:10:49 -0300

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,6 @@ const pool = new Pool({
   connectionString: 'postgresql://bd_precocerto_user:Lptizk3SMOVsutR6MlqsisbsLyjfYHgY@dpg-d1m8elmmcj7s739vqot0-a/bd_precocerto'
 });
 
-
-
-// // CONECTANDO NO BANCO LOCAL
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'bdPrecoCerto',
-//     password: '123',
-//     port: 5432,
-// });
-
 pool.connect()
   .then(client => {
     console.log('Conectado ao PostgreSQL');
@@ -30,18 +19,21 @@ pool.connect()
   })
   .catch(err => console.error('Erro ao conectar no PostgreSQL', err.stack));
 
+app.get('/', (req, res) => {
+  res.send('API PrecoCerto rodando!');
+});
 
 app.get('/produtos', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM VW_PRODUTOS_CATEGORIAS');
-        res.json(result.rows);
-    } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
-        res.status(500).send('Erro no servidor');
-    }
+  try {
+    const result = await pool.query('SELECT * FROM VW_PRODUTOS_CATEGORIAS');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    res.status(500).send('Erro no servidor');
+  }
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
