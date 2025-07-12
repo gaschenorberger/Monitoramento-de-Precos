2ad4e11d639ff0f1542c6af0e363dbf841697e2c
Gabriel Alvise <schenorberger123@gmail.com>
2025-07-12 00:26:46 -0300

    ALTERAÇÃO API PARA LOCAL

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,63 @@
-import express from 'express';
-import cors from 'cors';
-import pkg from 'pg';
+// WEB
 
-const { Pool } = pkg;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// import express from 'express';
+// import cors from 'cors';
+// import pkg from 'pg';
+
+// const { Pool } = pkg;
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
 // const pool = new Pool({
 //   connectionString: 'postgresql://bd_precocerto_user:Lptizk3SMOVsutR6MlqsisbsLyjfYHgY@dpg-d1m8elmmcj7s739vqot0-a/bd_precocerto'
 // });
 
 
-// Conexão com PostgreSQL
+// pool.connect()
+//   .then(client => {
+//     console.log('Conectado ao PostgreSQL');
+//     client.release();
+//   })
+//   .catch(err => console.error('Erro ao conectar no PostgreSQL', err.stack));
+
+// app.get('/', (req, res) => {
+//   res.send('API PrecoCerto rodando!');
+// });
+
+// app.get('/produtos', async (req, res) => {
+//   try {
+//     const result = await pool.query('SELECT * FROM VW_PRODUTOS_CATEGORIAS');
+//     res.json(result.rows);
+//   } catch (error) {
+//     console.error('Erro ao buscar produtos:', error);
+//     res.status(500).send('Erro no servidor');
+//   }
+// });
+
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+
+
+//-----------------------------------
+
+//LOCAL
+
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// CONECTANDO NO BANCO
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -29,26 +73,16 @@ pool.connect()
   })
   .catch(err => console.error('Erro ao conectar no PostgreSQL', err.stack));
 
-app.get('/', (req, res) => {
-  res.send('API PrecoCerto rodando!');
-});
-
 app.get('/produtos', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM VW_PRODUTOS_CATEGORIAS');
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Erro ao buscar produtos:', error);
-    res.status(500).send('Erro no servidor');
-  }
+    try {
+        const result = await pool.query('SELECT * FROM VW_PRODUTOS_CATEGORIAS');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+        res.status(500).send('Erro no servidor');
+    }
 });
 
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
-// SERVIDOR LOCAL
 app.listen(3001, () => {
     console.log('Servidor rodando na porta 3001');
 });
