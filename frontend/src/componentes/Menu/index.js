fc6625d6f6b62e1e0206257798721ba8f87c15ae
João Gnoatto <joaogabrielgnoatto2@gmail.com>
2025-07-14 12:08:08 -0300

    HTML E CSS
    Inicio da responsividade

--- frontend/src/componentes/Menu/index.js
+++ frontend/src/componentes/Menu/index.js
@@ -11,7 +11,7 @@ function Menu(){
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
                     </svg>
-                    XXXXX-XXX <br /> Endereço do usuário
+                    XXXXX-XXX <br /> Insira seu CEP
                 </a>
                 </li>
 
--- frontend/src/componentes/Titulo/index.js
+++ frontend/src/componentes/Titulo/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Titulo = styled.h2`
-    margin-left: 15%;
+    margin-left: 17%;
     justify-content: center;
     margin-top: 71px;
     font-size: ${props => props.tamanhoFonte};
